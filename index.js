1750f2b5dd25a1d7f73f882187a30f57886cce73
Ivan Markevich <hitlerwasdoubleright@gmail.com>
2021-04-20 08:37:08 +0300

    create basic Electron app

--- index.js
+++
@@ -1,9 +0,0 @@
-const magmaCypher = require('./build/Release/magma_cypher');
-const message = require('./config.json').message;
-
-let originalStr = message;
-console.log("original: " + originalStr);
-let encryptedStr = magmaCypher.encrypt(originalStr);
-console.log("encrypted: " + encryptedStr);
-let decryptedStr = magmaCypher.decrypt(encryptedStr);
-console.log("decrypted: " + decryptedStr);
